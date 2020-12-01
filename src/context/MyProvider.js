1c068876fdaf0dee6e10c9f2fc72974ea8b15270
Teiko <teikomancini1993@gmail.com>
2020-12-01 22:00:42 +0100

    commit before i mess up

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -5,24 +5,23 @@ export const MyContext = React.createContext();
 class MyProvider extends Component {
   state = {
     squares: [
-      { name: 0, isClicked: false, playerClicked: 0 },
-      { name: 1, isClicked: false, playerClicked: 0 },
-      { name: 2, isClicked: false, playerClicked: 0 },
-      { name: 3, isClicked: false, playerClicked: 0 },
-      { name: 4, isClicked: false, playerClicked: 0 },
-      { name: 5, isClicked: false, playerClicked: 0 },
-      { name: 6, isClicked: false, playerClicked: 0 },
-      { name: 7, isClicked: false, playerClicked: 0 },
-      { name: 8, isClicked: false, playerClicked: 0 },
+      { name: 0, isClicked: false, playerClicked: false },
+      { name: 1, isClicked: false, playerClicked: false },
+      { name: 2, isClicked: false, playerClicked: false },
+      { name: 3, isClicked: false, playerClicked: false },
+      { name: 4, isClicked: false, playerClicked: false },
+      { name: 5, isClicked: false, playerClicked: false },
+      { name: 6, isClicked: false, playerClicked: false },
+      { name: 7, isClicked: false, playerClicked: false },
+      { name: 8, isClicked: false, playerClicked: false },
     ],
-    player: 0
+    player: [0, 0, 0, 0, 0, 0, 0, 0, 0]
   };
   click = (pizza) => {
       let newSquares= this.state.squares.slice();
-      let playerClicked = this.state.player===0 ? 1 : 2
+      
       pizza.isClicked= true;
-      pizza.playerClicked= playerClicked
-      this.setState({squares: newSquares, player: playerClicked}
+      this.setState({squares: newSquares, player: this.state.player? 0: 1}
         )
   }
 /* NOT BREAKING CODE */
